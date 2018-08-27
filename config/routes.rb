902c245c47b68e05cfc0fab02f22ac55234efe72
Pan Cheng <pchengeducation@hotmail.com>
2018-08-27 16:14:42 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # However for the sake of this lab and seeing
   # how you can pass params through the
   # route, we'll keep it explicit and refactor it later
-  
+
 
   #get "students/:id", to: "students#show"
 
